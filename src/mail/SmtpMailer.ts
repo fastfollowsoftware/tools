70f66297275ddc6acfda786f6ed756b5e9b49fbf
David Maksimov <dmaksi@gmail.com>
2025-08-08 20:45:06 -0500

    fix smtp mailer auth

--- src/mail/SmtpMailer.ts
+++ src/mail/SmtpMailer.ts
@@ -1,21 +1,28 @@
 import { SmtpConfig } from './Config';
 import { MailSendOptions } from './MailSendOptions';
-import nodemailer from 'nodemailer';
+import nodemailer, { TransportOptions } from 'nodemailer';
 import { MailerInterface } from './Mailer.interface';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 export class SmtpMailer implements MailerInterface {
   private mailer;
 
   constructor(config: SmtpConfig) {
-    this.mailer = nodemailer.createTransport({
+    const transportConfig: SMTPTransport.Options = {
       host: config.host,
       port: config.port,
       secure: config.tls,
-      auth: {
+    };
+
+    // Only add auth if username and password are provided
+    if (config.username && config.password) {
+      transportConfig.auth = {
         user: config.username,
         pass: config.password,
-      },
-    });
+      };
+    }
+
+    this.mailer = nodemailer.createTransport(transportConfig);
   }
 
   async send(options: MailSendOptions) {
